cefce31c6e1579b73a8d2948c7d13debd1a4bc6b
dantegarden <761043617@qq.com>
2019-06-24 22:50:14 +0800

    测试

--- ssm-web/src/main/java/com/letinvr/web/controller/UserController.java
+++ ssm-web/src/main/java/com/letinvr/web/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.letinvr.web.controller;
 
+import com.letinvr.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("")
-    public String test(){
-        return "success";
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/test")
+    public ResponseEntity test(){
+
+        return ResponseEntity.ok(userService.findAll());
     }
 }
